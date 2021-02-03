fc1b5b6b3e55df395be52197f53e171b95ef87ac
Constantin Gabriel <39054700+Kira060200@users.noreply.github.com>
2021-02-03 14:39:21 +0200

    Added median and target for total wasted time

--- line_chart.py
+++ line_chart.py
@@ -6,7 +6,7 @@ time_filepath = "./wasted_time_on_social_media.csv"
 
 time_data = pd.read_csv(time_filepath, index_col="Date")
 
-print(time_data.head())
+plt.figure(figsize=(10, 5))
 sns.lineplot(data=time_data)
 plt.xticks(
     rotation=45,
@@ -22,7 +22,8 @@ plt.savefig("wasted_time.png", bbox_inches='tight', dpi = 300)
 plt.clf()
 
 time_data['Total'] = time_data['Reddit'] + time_data['Wapp'] + time_data['Fb'] + time_data['Chrome']
-sns.lineplot(data=time_data['Total'])
+plt.figure(figsize=(10, 5))
+sns.lineplot(data=time_data['Total'], label="Time wasted per day")
 plt.xticks(
     rotation=45,
     horizontalalignment='right',
@@ -32,5 +33,9 @@ plt.xticks(
 
 plt.ylabel('Minutes')
 # plt.show()
+plt.axhline(y=50, xmin=-1, xmax=1, color='orange', linestyle='--', lw=2, label="Target time")
+plt.axhline(y=time_data['Total'].median(), xmin=-1, xmax=1, color='r', linestyle='--', lw=1, label="Actual median")
+# plotting the legend 
+plt.legend(bbox_to_anchor = (1.0, 1), loc = 'upper center') 
 plt.savefig("total_wasted_time.png", bbox_inches='tight', dpi = 300)
 
